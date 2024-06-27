c1a50e90aa5119285a88a7ecf64edc08b4af6c4f
Akash kumar maurya <akashmaurya09994@gmail.com>
2024-06-28 00:45:19 +0530

    gfg solution

--- Array3/560_SubarraySumEqualsK.cpp
+++ Array3/560_SubarraySumEqualsK.cpp
@@ -26,3 +26,32 @@ public:
 // but if sum-k already map add into the result 
 // concep ye h ki sum-k==0 hua matlb sum ==k ho gya agar aage b sum-k some how phle se mapped hua h to matlb sum me k aur add hua hoga
 // ya to aise smj lo ki i to k sum ==k , k to n k bich me sum==k ho sakta h wo b subarray add karna h 
+
+
+// same code little differe of gfg 
+class Solution {
+  public:
+    int maxLen(vector<int>& nums, int n) {
+        // Your code here
+       
+        unordered_map<int,int>mp;
+        int maxi = 0; 
+        mp[0] =-1;
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=nums[i];
+            if(mp.find(sum)!=mp.end())
+            {
+                int index = i-mp[sum];
+                maxi=max(index , maxi);
+            }
+            else
+            {
+              mp[sum] =i;
+            }
+            
+        }
+        return maxi;
+    }
+};
