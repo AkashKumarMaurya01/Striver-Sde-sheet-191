2665b7a61cfbfeaa36dd6b8e7fc148369bfa3e99
Akash kumar maurya <akashmaurya09994@gmail.com>
2024-06-27 13:50:51 +0530

    longest consequence

--- Array3/128_longest_common_subsequence.cpp
+++ Array3/128_longest_common_subsequence.cpp
@@ -27,3 +27,41 @@ public:
         return maxi;
     }
 };
+
+// Second solution O(N) and O(N);
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+
+        unordered_set<int>st;
+        for(auto i:nums) st.insert(i);
+         int maxi = 0;
+        for(auto i :st)
+        {
+            if(st.find(i-1)==st.end())
+            {
+                int count=1;
+                int x = i+1;
+                while(st.find(x)!=st.end())
+                {
+                    count++;
+                    x+=1;
+                }
+                maxi = max(maxi , count);
+            }
+        }
+        return maxi;   
+    }
+};
+
+/* 
+ Approach : first we put all the element into the set for searching unique element 
+           : second we find a element in the set in which the element less then 1 
+              100 , 200 , 1 , 3 , 4 , 2 
+              example: is (100-1)==99 present or not -->its not present move furthur and then find 100+1 presend or not if yes count++ else go to next 
+                      is (200-1)==0 present or not -->its not present move furthur and then find 100+1 presend or not if yes count++ else go to next 
+                      similary 1-1 ==0 present ->its not present go to search for seach x= 1+1 , , x=x+1 , while it is presnt count++ 
+                      
+                   1 ,2, 3, 4 presnt count =4 
+                   max = 4 answer
+*/
